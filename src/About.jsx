1952ec2a80f2947146bb699a403304cf6eaad8f5
Artnalyze <artnalyze@gmail.com>
2021-11-29 01:10:48 +0700

    update sass

---
+++ src/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>Live สด</h1>
+            <h2>ล่าสุด</h2>
+            <div style={{color:'green', fontSize: 27}}>เวลาเป็นเครื่องพิสูจน์ ใบมะกรูดเป็นเครื่องต้มยำ</div>
+            <p style={{
+                color: 'blue',
+                fontSize: 18,
+                border: '1px solid red',
+                textAlign: 'left',
+                padding: 20
+            }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati perspiciatis ea qui quo, iure repellendus distinctio atque quasi eius culpa.</p>
+        </div>
+    )
+}
+
+export default About
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './MyStyle.css'
+import About from './About';
 
 function App() {
+  const myName = 'Art dvp'
+  const mySalary = 30000
+  const mySisterSalary = 10000
+  const randomNumber = Math.random()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='App'>Section A</div>
+      <About/>
     </div>
   );
 }
